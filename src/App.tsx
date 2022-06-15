4beed8f0d5ea87c5a52798bf081e0a58e1a15840
Matheus Costa <matheuscastiglia31@gmail.com>
2022-06-15 09:09:09 -0300

    Front-end concluído

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Widget from "./components/Widget"
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <div>
+      <Widget />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/WidgetForm/Loading/index.tsx
@@ -0,0 +1,13 @@
+import { CircleNotch } from "phosphor-react";
+
+type Props = {};
+
+const Loading = (props: Props) => {
+  return (
+    <div className="w-6 h-6 flex items-center justify-center overflow-hidden">
+      <CircleNotch weight="bold" className="w-4 h-4 animate-spin" />
+    </div>
+  );
+};
+
+export default Loading;
